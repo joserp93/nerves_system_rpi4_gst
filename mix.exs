04717e487a081398d9185999047e530552c1a432
Jose Rodriguez <josev01@gmail.com>
2024-07-10 12:48:40 -0400

    add gst custom

--- mix.exs
+++ mix.exs
@@ -1,8 +1,8 @@
-defmodule NervesSystemRpi4.MixProject do
+defmodule NervesSystemRpi4Gst.MixProject do
   use Mix.Project
 
-  @github_organization "nerves-project"
-  @app :nerves_system_rpi4
+  @github_organization "joserp93"
+  @app :nerves_system_rpi4_gst
   @source_url "https://github.com/#{@github_organization}/#{@app}"
   @version Path.join(__DIR__, "VERSION")
            |> File.read!()
